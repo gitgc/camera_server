98c428b0264ea71ce44e5ad5d93a200eff7b1402
Giovanni Coia <g.coia@icloud.com>
2016-03-08 18:03:10 -0800

    adding paw file for api testing

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -3,13 +3,11 @@ var cameraServer = angular.module('cameraServer', ['ngRoute', 'eventsController'
 cameraServer.config(function($routeProvider) {
     $routeProvider
 
-        // route for the home page
         .when('/', {
             templateUrl : '/views/home',
             controller  : 'EventListController'
         })
 
-        // route for the about page
         .when('/event/:eventId', {
             templateUrl : '/views/event',
             controller  : 'EventDetailController'
